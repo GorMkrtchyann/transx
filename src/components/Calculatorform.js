c8b23499745d7695ae80e8b3bda42f3b1b9a69a8
Hovo <hovhannes.varosyan.02@mail.ru>
2024-03-08 02:53:58 +0400

    fixrd bugs

--- src/components/Calculatorform.js
+++ src/components/Calculatorform.js
@@ -12,8 +12,8 @@ function CalculatorForm({ i, e, form, setForm }) {
             <h6 className="calculator-form__title">Quote {i + 1}</h6>
             <div className="row bottom-20" value={e.value} key={i}>
                 <div className="col-sm-4">
-                    <select className="form__select nice-select " name="origin" onChange={(e) => { handleChange(e, i,) }}>
-                        <option data-display=" Country of origin">Country of origin </option>
+                    <select className="form__select nice-select " name="origin" onChange={(e) => { handleChange(e, i,) }} required>
+                        <option value="">Country of origin </option>
                         <option value="value">Value 1</option>
                         <option value="value2">Value 2</option>
                         <option value="value3">Value 3</option>
@@ -22,8 +22,8 @@ function CalculatorForm({ i, e, form, setForm }) {
                     </select>
                 </div>
                 <div className="col-sm-4">
-                    <select className="form__select nice-select" value={e.value} name="delivery" onChange={(e) => { handleChange(e, i,) }}>
-                        <option data-display="Country of delivery">Country of delivery </option>
+                    <select className="form__select nice-select" value={e.value} name="delivery" onChange={(e) => { handleChange(e, i,) }} required>
+                        <option value="">Country of delivery </option>
                         <option value="value">Value 1</option>
                         <option value="value2">Value 2</option>
                         <option value="value3">Value 3</option>
@@ -32,8 +32,8 @@ function CalculatorForm({ i, e, form, setForm }) {
                     </select>
                 </div>
                 <div className="col-sm-4">
-                    <select className="form__select nice-select" value={e.value} name="service" onChange={(e) => { handleChange(e, i,) }}>
-                        <option data-display="Type of service">Type of service </option>
+                    <select className="form__select nice-select" value={e.value} name="service" onChange={(e) => { handleChange(e, i,) }} required>
+                        <option value="">Type of service </option>
                         <option value="value">Value 1</option>
                         <option value="value2">Value 2</option>
                         <option value="value3">Value 3</option>
@@ -42,16 +42,16 @@ function CalculatorForm({ i, e, form, setForm }) {
                     </select>
                 </div>
                 <div className="col-sm-6 col-md-3">
-                    <input className="form__field" type="number" value={e.weight} name="weight" onChange={(e) => { handleChange(e, i,) }} placeholder="Weight (kg)" />
+                    <input className="form__field" type="number" value={e.weight} name="weight" onChange={(e) => { handleChange(e, i,) }} placeholder="Weight (kg)" required />
                 </div>
                 <div className="col-sm-6 col-md-3">
-                    <input className="form__field" type="number" value={e.height} name="height" onChange={(e) => { handleChange(e, i,) }} placeholder="Height (cm)" />
+                    <input className="form__field" type="number" value={e.height} name="height" onChange={(e) => { handleChange(e, i,) }} placeholder="Height (cm)" required />
                 </div>
                 <div className="col-sm-6 col-md-3">
-                    <input className="form__field" type="number" value={e.width} name="width" onChange={(e) => { handleChange(e, i,) }} placeholder="Width (cm)" />
+                    <input className="form__field" type="number" value={e.width} name="width" onChange={(e) => { handleChange(e, i,) }} placeholder="Width (cm)" required />
                 </div>
                 <div className="col-sm-6 col-md-3">
-                    <input className="form__field" type="number" value={e.length} name="lenght" onChange={(e) => { handleChange(e, i,) }} placeholder="Lenght (cm)" />
+                    <input className="form__field" type="number" value={e.length} name="length" onChange={(e) => { handleChange(e, i,) }} placeholder="Lenght (cm)" required />
                 </div>
             </div>
         </>
