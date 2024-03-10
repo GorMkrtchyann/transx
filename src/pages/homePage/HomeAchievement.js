ce94012602d0d43eb26260809d41e37b4cdfc803
Hovhannisyan2112 <hovhannisyan.2112@mail.ru>
2024-03-11 01:27:37 +0400

    Homepage map image updating

--- src/pages/homePage/HomeAchievement.js
+++ src/pages/homePage/HomeAchievement.js
@@ -5,7 +5,7 @@ const HomeAchievement = (props) => {
         <section className="section">
             <div className="container">
                 <div className="row">
-                    <div className="col-12"><img src="img/map-transx.png" alt="img"/></div>
+                    <div className="col-12 map-box"><img src="img/achievement-map.png" alt="img"/></div>
                 </div>
                 <div className="row offset-50">
                     <div className="col-md-6 col-lg-4 text-center">
